0bc6e29402fff81f0af33c7b3013ce0fad644cf4
Nate Simon <nsimon@ucvts.tec.nj.us>
2013-04-18 16:33:46 -0400

    10

---
+++ Team1257MoreCode/Team1257Robot.h
@@ -0,0 +1,28 @@
+#include <WPIlib.h>
+#define CHANNEL_PING new DigitalOutput(1)
+#define CHANNEL_ECHO new DigitalInput(2)
+#define LEFT_MOTOR 1
+#define RIGHT_MOTOR 2
+#define LEFT_STICK 1
+#define RIGHT_STICK 2
+#define GYRO 3
+class CTeam1257Robot
+{
+public:
+	CTeam1257Robot();
+	void Autonomous();
+	void OperatorControl();
+	void Test();
+	void Drive();
+	void Drive(int,int);
+	void Turn();
+	void Approx(int, int, int);
+public:
+	Ultrasonic ultrasonic;
+	RobotDrive team1257Robot;
+	Joystick leftStick;
+	Joystick rightStick;
+	DriverStationLCD* team1257LCD;
+	Gyro gyro;
+	int tolerance;
+};
