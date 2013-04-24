23b3e1fe29689dcd56e803b5bea0a4d9655415e6
Nathan Simon <nsimon@ucvts.tec.nj.us>
2013-04-24 11:09:55 -0400

    final fix

--- .metadata/.plugins/com.windriver.ide.core/compilerInspection.cpp.d
+++ .metadata/.plugins/com.windriver.ide.core/compilerInspection.cpp.d
@@ -1,2 +1,2 @@
 compilerInspection.o:  \
- E:/GitHub/Team1257Code/.metadata/.plugins/com.windriver.ide.core/compilerInspection.cpp
+ Z:/GitHub/Team1257Code/.metadata/.plugins/com.windriver.ide.core/compilerInspection.cpp
---
+++ Team1257MoreCode/README.txt
@@ -0,0 +1,2 @@
+This is my test code. I haven't tested it yet but it should work. Raggle Fraggle!
+-Nathan
